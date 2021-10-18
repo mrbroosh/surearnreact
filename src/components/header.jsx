6802aaa9a4d27ed77cf71312454b90f1856d9c41
BRUNOCHERUIYOT <50874885+BRUNOCHERUIYOT@users.noreply.github.com>
2021-10-18 14:37:21 +0300

    first commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -23,23 +23,14 @@ class Header extends Component {
             {/* <!-- <li><a className="nav-link scrollto" href="#team">Scores</a></li> --> */}
             <li className="dropdown"><a href="#"><span>Match tips</span> <i className="bi bi-chevron-down"></i></a>
               <ul>
-                <li><a href="#">Daily tip</a></li>
-                <li className="dropdown"><a href="#"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></a>
-                  <ul>
-                    <li><a href="#">Deep Drop Down 1</a></li>
-                    <li><a href="#">Deep Drop Down 2</a></li>
-                    <li><a href="#">Deep Drop Down 3</a></li>
-                    <li><a href="#">Deep Drop Down 4</a></li>
-                    <li><a href="#">Deep Drop Down 5</a></li>
-                  </ul>
-                </li>
-                <li><a href="#">Weekend fixed tip</a></li>
-                <li><a href="#">Special fixed tip</a></li>
-                <li><a href="#">Drop Down 4</a></li>
+                <li><a href="#">Daily tip -> Ksh.100 </a></li>
+                <li><a href="#">Weekend fixed tip -> ksh.200</a></li>
+                <li><a href="#">Special fixed tip -> ksh.500</a></li>
+                
               </ul>
             </li>
             <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
-            <li><a className="getstarted scrollto" href="#about">Login</a></li>
+            <li><a className="getstarted scrollto" href="#about">Mpesa No: 9139603</a></li>
           </ul>
           <i className="bi bi-list mobile-nav-toggle"></i>
         </nav>
